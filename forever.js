cc6a57b62e934096163006eb8d001591dbd3e809
equaliser <accounts@equaliser.minimalcity.de>
2019-10-22 19:02:28 +0200

    Remove Forever-Monitor Module completely - 0.1.1

--- forever.js
+++
@@ -1,9 +0,0 @@
-const forever = require('forever-monitor');
-
-const child = new forever.Monitor('index.js');
-
-child.on('exit', function () {
-  console.log('Server has exited!');
-});
-
-child.start();
