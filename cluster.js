81b2bf1434bd44dc7d93eb9bbdb792c50987aeb7
equaliser0 <accounts@equaliser.minimalcity.de>
2019-11-19 10:20:23 +0100

    Implement Cluster

---
+++ cluster.js
@@ -0,0 +1,16 @@
+let cluster = require('cluster');
+
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  let cpuCount = require('os').cpus().length;
+  // Create a worker for each CPU
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+  // Listen for dying workers
+  cluster.on('exit', function () {
+    cluster.fork();
+  });
+} else {
+  require('./index');
+}
