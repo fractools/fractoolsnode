4bc7012be086323d034753ce9c075bf25070bd41
consuman <nicolai@consuman.de>
2019-11-26 13:59:43 +0100

    small change in logger - "Put Data" to "Add Data"

--- sockets/dbmanagement.js
+++ sockets/dbmanagement.js
@@ -19,11 +19,11 @@ module.exports = (socket, clients) => {
     } catch (err) {
       fn(err, null);
     }
-  })
+  });
 
   // Fetch Documents to/from Remote
   socket.on(`last-documents`, async (database, fn) => {
-    console.dir(` ######## [ Server Socket ] ######## Fetch Data from "${database}"`)
+    console.dir(` ######## [ Server Socket ] ######## Fetch Data from "${database}"`);
     try {
       let docs = await fetch(database);
       fn(null, docs);
@@ -35,85 +35,29 @@ module.exports = (socket, clients) => {
     } catch (err) {
       console.log(err);
     }
-  })
+  });
 
   // Send and Broadcast new Document
   socket.on(`send-document`, async (database, data, id, fn) => {
-    console.dir(` ######## [ Server Socket ] ######## Put Data in "${database}"`)
-    let client = clients.find(client => client.id === socket.id)
+    console.dir(` ######## [ Server Socket ] ######## Add new Data in "${database}"`);
+    let client = clients.find(client => client.id === socket.id);
 
 
-    let db = new PouchDB(`./database/${database}`)
+    let db = new PouchDB(`./database/${database}`);
     let doc = {
       _id: id,
       ...data
-    }
+    };
     try {
-      let response = await db.put(doc)
-      fn(null, response)
-      let docs = await fetch(database);
-      socket.broadcast.emit(`documents`, docs, database)
-      socket.emit(`documents`, docs, database)
-      logger('Documents', 'info', `Put Data in "${database}"`, client)
-    } catch (err) {
-      console.log(err);
-      logger('Documents', 'error', `Error Putting Data in "${database}": ${err}`, client)
-      fn(err, null)
-    }
-    try {
-      await replicate(database);
-    } catch (err) {
-      console.log(err);
-    }
-  })
-
-  // Send and Broadcast updated Document
-  socket.on(`update-document`, async (database, data, id, rev, fn) => {
-    console.dir(` ######## [ Server Socket ] ######## Update Data in "${database}"`)
-    let client = clients.find(client => client.id === socket.id)
-    let db = new PouchDB(`./database/${database}`)
-    let docs;
-    try {
-      let doc = await db.get(id)
-      let response = await db.put({
-        _id: id,
-        _rev: doc._rev,
-        ...doc = data
-      })
-      docs = await fetch(database);
+      let response = await db.put(doc);
       fn(null, response);
+      let docs = await fetch(database);
       socket.broadcast.emit(`documents`, docs, database);
       socket.emit(`documents`, docs, database);
-      logger('Documents', 'info', `Update Data in "${database}"`, client)
-    } catch (err) {
-      console.dir(err);
-      logger('Documents', 'error', `Error Updating Data in "${database}": ${err}`, client)
-      fn(err, null)
-    }
-    try {
-      await replicate(database);
+      logger('Documents', 'info', `Add new Data in "${database}"`, client);
     } catch (err) {
       console.log(err);
-    }
-  })
-
-  // Remove and Broadcast removed Document
-  socket.on(`remove-document`, async (database, obj, fn) => {
-    console.dir(` ######## [ Server Socket ] ######## Remove Data in "${database}"`)
-    let client = clients.find(client => client.id === socket.id)
-    let db = new PouchDB(`./database/${database}`)
-    let docs;
-    try {
-      let doc = await db.get(obj._id)
-      let response = await db.remove(doc);
-      docs = await fetch(database);
-      fn(null, response);
-      socket.broadcast.emit(`documents`, docs, database)
-      socket.emit(`documents`, docs, database)
-      logger('Documents', 'info', `Remove Data in "${database}"`, client)
-    } catch (err) {
-      console.dir(err);
-      logger('Documents', 'error', `Error Removing Data in "${database}": ${err}`, client)
+      logger('Documents', 'error', `Error adding new Data in "${database}": ${err}`, client);
       fn(err, null);
     }
     try {
@@ -121,43 +65,99 @@ module.exports = (socket, clients) => {
     } catch (err) {
       console.log(err);
     }
-  })
+  });
+
+  // Send and Broadcast updated Document
+  socket.on(`update-document`, async (database, data, id, rev, fn) => {
+    console.dir(` ######## [ Server Socket ] ######## Update Data in "${database}"`);
+    let client = clients.find(client => client.id === socket.id);
+    let db = new PouchDB(`./database/${database}`);
+    let docs;
+    try {
+      let doc = await db.get(id);
+      let response = await db.put({
+        _id: id,
+        _rev: doc._rev,
+        ...doc = data
+      });
+      docs = await fetch(database);
+      fn(null, response);
+      socket.broadcast.emit(`documents`, docs, database);
+      socket.emit(`documents`, docs, database);
+      logger('Documents', 'info', `Update Data in "${database}"`, client);
+    } catch (err) {
+      console.dir(err);
+      logger('Documents', 'error', `Error Updating Data in "${database}": ${err}`, client);
+      fn(err, null);
+    }
+    try {
+      await replicate(database);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // Remove and Broadcast removed Document
+  socket.on(`remove-document`, async (database, obj, fn) => {
+    console.dir(` ######## [ Server Socket ] ######## Remove Data in "${database}"`);
+    let client = clients.find(client => client.id === socket.id);
+    let db = new PouchDB(`./database/${database}`);
+    let docs;
+    try {
+      let doc = await db.get(obj._id);
+      let response = await db.remove(doc);
+      docs = await fetch(database);
+      fn(null, response);
+      socket.broadcast.emit(`documents`, docs, database);
+      socket.emit(`documents`, docs, database);
+      logger('Documents', 'info', `Remove Data in "${database}"`, client);
+    } catch (err) {
+      console.dir(err);
+      logger('Documents', 'error', `Error Removing Data in "${database}": ${err}`, client);
+      fn(err, null);
+    }
+    try {
+      await replicate(database);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
   // Get Single Document
   socket.on(`get-document`, async (database, id, fn) => {
-    console.dir(` ######## [ Server Socket ] ######## Get Single Doc from "${database}"`)
-    let db = new PouchDB(`./database/${database}`)
+    console.dir(` ######## [ Server Socket ] ######## Get Single Doc from "${database}"`);
+    let db = new PouchDB(`./database/${database}`);
     try {
-      let doc = await db.get(id)
-      fn(null, doc)
+      let doc = await db.get(id);
+      fn(null, doc);
     } catch (err) {
       console.dir(err);
-      fn(err, null)
+      fn(err, null);
     }
-  })
+  });
 
   // Send and Broadcast new Document
   socket.on(`send-db`, async (data, id, fn) => {
-    console.dir(` ######## [ Server Socket ] ######## Put ${data.dbname} in "Databases"`)
-    let client = clients.find(client => client.id === socket.id)
-    let db = new PouchDB(`./database/databases`)
+    console.dir(` ######## [ Server Socket ] ######## Put ${data.dbname} in "Databases"`);
+    let client = clients.find(client => client.id === socket.id);
+    let db = new PouchDB(`./database/databases`);
     let doc = {
       _id: id,
       ...data
     };
     let docs;
     try {
-      let response = await db.put(doc)
+      let response = await db.put(doc);
       docs = await fetch('databases');
       fn(null, response);
-      socket.broadcast.emit(`new-database`, doc)
-      socket.emit(`new-database`, doc)
-      socket.broadcast.emit(`documents`, docs, 'databases')
-      socket.emit(`documents`, docs, 'databases')
-      logger('Documents', 'info', `Put "${data.dbname}" into "Databases"`, client)
+      socket.broadcast.emit(`new-database`, doc);
+      socket.emit(`new-database`, doc);
+      socket.broadcast.emit(`documents`, docs, 'databases');
+      socket.emit(`documents`, docs, 'databases');
+      logger('Documents', 'info', `Put "${data.dbname}" into "Databases"`, client);
     } catch (err) {
       console.log(err);
-      logger('Documents', 'error', `Error Putting "${data.dbname}" in "Databases": ${err}`, client)
+      logger('Documents', 'error', `Error Putting "${data.dbname}" in "Databases": ${err}`, client);
       fn(err, null);
     }
     try {
@@ -165,22 +165,22 @@ module.exports = (socket, clients) => {
     } catch (err) {
       console.log(err);
     }
-  })
+  });
 
   // Replicate Documents to/from Remote
   socket.on(`replicate-database`, async (database) => {
-    console.dir(` ######## [ Server Socket ] ######## Replicate Data for "${database}"`)
-    let db = new PouchDB(`./database/${database}`)
+    console.dir(` ######## [ Server Socket ] ######## Replicate Data for "${database}"`);
+    let db = new PouchDB(`./database/${database}`);
     await replicate(db, database);
-  })
+  });
 
   // Replicate Documents to/from Remote
   socket.on(`replicateFT`, async (server1, database1, server2, database2) => {
-    console.dir(` ######## [ Server Socket ] ######## Replicate from "${database1}" to "${database2}"`)
-    let db = new PouchDB(`./database/${database1}`)
+    console.dir(` ######## [ Server Socket ] ######## Replicate from "${database1}" to "${database2}"`);
+    let db = new PouchDB(`./database/${database1}`);
     await db.replicate.from(`http://${server1}/${database1}`);
     await db.replicate.to(`http://${server2}/${database2}`);
     let db2 = new PouchDB(`./database/${database2}`);
     await db2.replicate.from(`http://${server2}/${database2}`);
-  })
+  });
 }
